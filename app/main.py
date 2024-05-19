3f13ff802cf9768cb53d9c6d2b6aa84984d3bdcd
AndreiVed <catblues87@gmail.com>
2024-05-20 00:28:43 +0300

    create two clases and two methods

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Car:
-    # write your code here
-    pass
-
-
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(
+        self,
+        distance_from_city_center: int,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
+    ):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, cars: list) -> list:
+        result = []
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                car.clean_mark = self.clean_power
+            result += [car.clean_mark]
+        return result
+
+    def calculate_washing_price(self, car: dict) -> float:
+        price = car.comfort_class * (self.clean_power - car.clean_mark) * self.average_rating / self.distance_from_city_center
+        return round(price)
